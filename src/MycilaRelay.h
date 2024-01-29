d2b1c285f1d0193e6252eb636b43c51993a29d1b
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-01-29 22:32:12 +0100

    v1.1.0

--- src/MycilaRelay.h
+++ src/MycilaRelay.h
@@ -13,10 +13,10 @@
 #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_RELAY_VERSION "1.0.2"
+#define MYCILA_RELAY_VERSION "1.1.0"
 #define MYCILA_RELAY_VERSION_MAJOR 1
-#define MYCILA_RELAY_VERSION_MINOR 0
-#define MYCILA_RELAY_VERSION_REVISION 2
+#define MYCILA_RELAY_VERSION_MINOR 1
+#define MYCILA_RELAY_VERSION_REVISION 0
 
 namespace Mycila {
   enum class RelayType {
